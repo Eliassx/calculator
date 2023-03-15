7b4fe1e28a9bf38ed1363cbbc417a1498bf6e2ee
Guilherme Elias <124407357+Eliassx@users.noreply.github.com>
2023-03-15 09:28:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function insert(num) {
+    var numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+}
+function clean() {
+    document.getElementById('result').innerHTML = "";
+}
+function calculate() {
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(result);
+    }
+}
